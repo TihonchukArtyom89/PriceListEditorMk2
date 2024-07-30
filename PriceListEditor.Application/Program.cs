4c0fe5b826a56b8dfeeaf026121c0bee0c61ea3b
TihonchukArtyom89 <mortus1989@gmail.com>
2024-07-30 17:39:13 +0300

    change launchSettings json to configure http port, add folders, change Program cs to configure application for using some features

--- PriceListEditor.Application/Program.cs
+++ PriceListEditor.Application/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();//set up shared objects for MVC framweork
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();//use static content from wwwroot folder
+app.MapDefaultControllerRoute();//registers MVC framework as source of endpoint by using default convention of mapping requests to classes and methods
 
 app.Run();
