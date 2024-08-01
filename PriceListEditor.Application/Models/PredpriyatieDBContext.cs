5f5b997f950b4e67e43b7364cdc3645f6d0f0abd
TihonchukArtyom89 <mortus1989@gmail.com>
2024-08-01 19:23:59 +0300

    define connection string in appsettings json, add PredpriyatieDBContext cs (context class) to Models folder, change Program cs to configure EFCore

---
+++ PriceListEditor.Application/Models/PredpriyatieDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PriceListEditor.Application.Models;
+
+public class PredpriyatieDBContext : DbContext
+{
+    public PredpriyatieDBContext(DbContextOptions<PredpriyatieDBContext> options) : base(options) { }
+    DbSet<Product> Products => Set<Product>();
+}
--- PriceListEditor.Application/Program.cs
+++ PriceListEditor.Application/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PriceListEditor.Application.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();//set up shared objects for MVC framweork
+builder.Services.AddDbContext<PredpriyatieDBContext>(options => //allow connect to database by using connection string (defined in appsettings json)
+{//where objects and data mapped with hep of context class (PredpriyatieDBContext cs)
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:AppConnection"]);
+});
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
