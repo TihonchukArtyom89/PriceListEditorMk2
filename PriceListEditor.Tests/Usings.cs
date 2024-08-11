f675a44b753728cfc8fb67458c4fb80b9583eaf6
TihonchukArtyom89 <mortus1989@gmail.com>
2024-08-11 19:15:09 +0300

    rename UnitTest1 cs to ProductConrollerTest cs, add golbal usings to Usings cs, add test Can_Use_Repository to ProductConrollerTest cs

--- PriceListEditor.Tests/Usings.cs
+++ PriceListEditor.Tests/Usings.cs
@@ -1,1 +1,8 @@
 global using Xunit;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.AspNetCore.Mvc;
+global using Moq;
+global using PriceListEditor.Application.Controllers;
+global using PriceListEditor.Application.Models;
