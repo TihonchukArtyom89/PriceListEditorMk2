2a75dac8f361c7bbbf4d7f9d17ea7e77160c0791
TihonchukArtyom89 <mortus1989@gmail.com>
2024-08-20 18:53:21 +0300

    add class of page model (PageViewModel cs) to ViewModels folder, add class of  pagination and products on list (PageViewModel cs) to ViewModels folder

---
+++ PriceListEditor.Application/ViewModels/PageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PriceListEditor.Application.ViewModels;
+
+public class PageViewModel
+{
+    public int NumberOfPages { get; private set; }
+    public int TotalCountOfPages { get; private set; }
+    public PageViewModel(int count, int pageNumber, int pageSize)
+    {
+        NumberOfPages = pageNumber;
+        TotalCountOfPages = (int)Math.Ceiling(count / (double)pageSize);
+    }
+    public bool HasPreviousPage
+    {
+        get
+        {
+            return NumberOfPages > 1;
+        }
+    }
+    public bool HasNextPage
+    {
+        get
+        {
+            return NumberOfPages < TotalCountOfPages;
+        }
+    }
+}
---
+++ PriceListEditor.Application/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PriceListEditor.Application.Models;
+using System.Collections.Generic;
+
+namespace PriceListEditor.Application.ViewModels;
+
+public class ProductListViewModel
+{
+    public IEnumerable<Product>? Products { get; set; }
+    public PageViewModel? PageViewModel { get; set; }
+}
