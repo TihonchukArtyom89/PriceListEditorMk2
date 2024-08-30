879834f37c9f16b5f1952354f40b5de161dd60d7
TihonchukArtyom89 <mortus1989@gmail.com>
2024-08-30 20:05:15 +0300

    write both variants synchronous and asynchronous of method ProductList in (ProductController cs int Controllers folder)

--- PriceListEditor.Application/Controllers/ProductController.cs
+++ PriceListEditor.Application/Controllers/ProductController.cs
@@ -13,13 +13,29 @@ public class ProductController : Controller
     {
         productRepository = _productRepository;
     }
-    public ViewResult ProductList(int page = 1)
+
+    
+    //public ViewResult ProductList(int page = 1)//synchronous variany of method
+    //{
+    //    IQueryable<Product>? productSource = productRepository.Products; //source of products
+    //    int produtCount = productSource!.Count(); //total number of products
+    //    List<Product> pageProducts =  productSource!.Skip((page - 1)*pageSize).Take(pageSize).ToList();//list of products on a page
+    //    PageViewModel pageViewModel = new(page,pageSize, produtCount);
+    //    ProductListViewModel productListViewModel = new() 
+    //    {
+    //        PageViewModel = pageViewModel,
+    //        Products = pageProducts
+    //    };
+    //    return View(productListViewModel);
+    //}
+    
+    public async Task<IActionResult> ProductList(int page = 1)//asynchronous variant of method
     {
         IQueryable<Product>? productSource = productRepository.Products; //source of products
-        int produtCount = productSource!.Count(); //total number of products
-        List<Product> pageProducts =  productSource!.Skip((page - 1)*pageSize).Take(pageSize).ToList();//list of products on a page
-        PageViewModel pageViewModel = new(page,pageSize, produtCount);
-        ProductListViewModel productListViewModel = new() 
+        int produtCount = await productSource!.CountAsync(); //total number of products
+        List<Product> pageProducts = await productSource!.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();//list of products on a page
+        PageViewModel pageViewModel = new(page, pageSize, produtCount);
+        ProductListViewModel productListViewModel = new()
         {
             PageViewModel = pageViewModel,
             Products = pageProducts
--- PriceListEditor.Tests/ProductControllerTests.cs
+++ PriceListEditor.Tests/ProductControllerTests.cs
@@ -2,21 +2,28 @@ using PriceListEditor.Application.ViewModels;
 
 namespace PriceListEditor.Tests;
 
-public class ProductControllerTests 
+public class ProductControllerTests
 {
     [Fact]
     public void Can_Use_Repository()
     {
         //Arrange
         Mock<IProductRepository> mockRepository = new Mock<IProductRepository>();
+        //mockRepository.Setup(mr => mr.Products).Returns((new Product[]
+        //{
+        //    new Product{ProductID=1,ProductName="P1"},
+        //    new Product{ProductID=2,ProductName="P2"}
+        //}).AsQueryable<Product>());//for synchronous variat of ProductList method in ProductController
         mockRepository.Setup(mr => mr.Products).Returns((new Product[]
         {
             new Product{ProductID=1,ProductName="P1"},
             new Product{ProductID=2,ProductName="P2"}
-        }).AsQueryable<Product>());
+        }).AsQueryable<Product>());//for asynchronous variat of ProductList method in ProductController
         ProductController productController = new ProductController(mockRepository.Object);
         //Act
-        var resultProducts = (productController.ProductList()?.ViewData.Model as ProductListViewModel ?? new())?.Products?.ToArray() ?? Array.Empty<Product>();
+        //var resultProducts = (productController.ProductList()?.ViewData.Model as ProductListViewModel ?? new())?.Products?.ToArray() ?? Array.Empty<Product>();//for synchronous variat of ProductList method in ProductController
+        var resultProducts = (productController.ProductList()?.Result as ProductListViewModel ?? new())?.Products?.ToArray() ?? Array.Empty<Product>();//перерработать тест не проходитс€
+        //(productController.ProductList()?.ViewData.Model as ProductListViewModel ?? new())?.Products?.ToArray() ?? Array.Empty<Product>();
         //Assert
         Assert.True(resultProducts.Length == 2);//Assert.Equal(2, products.Length);//одинаково
         Assert.Equal("P1", resultProducts[0].ProductName);
