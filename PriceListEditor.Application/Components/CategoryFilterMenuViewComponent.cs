621f8f3f4fa4922cfa577754e521f199683550dd
TihonchukArtyom89 <mortus1989@gmail.com>
2024-09-26 18:41:04 +0300

    add category filter menu view component (CategoryFilterMenuViewComponent cs in Components folder, change view ProductList cshtml in Views/Product folder) to this view component

---
+++ PriceListEditor.Application/Components/CategoryFilterMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceListEditor.Application.Components;
+
+public class CategoryFilterMenuViewComponent :ViewComponent
+{
+    public string Invoke()
+    {
+        return "from view component";
+            
+    }
+}
